1c9895fe262fe3798f6486177d250a70a46e3111
Niko test <Lesnik6.6.6@mail.ru>
2023-05-17 20:25:33 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-
     public static void main(String[] args) {
+        int account = 1200;
+        int payment = 1100;
+        int bonusOne;
+        if (payment > 1000) {
+            bonusOne = 1;
+        } else {
+            bonusOne = 0;
+        }
+        int bonusTwo = 0;
+        if (payment < 1100) ;
+        int one = payment / 100;
+        int two = bonusOne * one + bonusTwo;
+        int end = two + account;
+        System.out.println("Итог:" + end);
     }
 }
